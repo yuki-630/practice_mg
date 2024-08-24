5eac2100946206b5a3d66e5fcb14e0003a05437e
yuki-630 <miki.yuki.omu@gmail.com>
2024-08-24 15:26:03 +0900

    ページネーション等(解答16)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,8 @@ class PostController extends Controller
 {
     public function index(Post $post) //インポートしｔPostをインスタンス化して＄postとして使用。
     {
-        return $post->get(); //$postの中身を戻り値にする。
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
     
 }
