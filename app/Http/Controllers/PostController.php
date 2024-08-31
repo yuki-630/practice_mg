b31e21d44af41ddb7989a0c205c6a22b24437cde
yuki-630 <miki.yuki.omu@gmail.com>
2024-08-31 21:55:22 +0900

    投稿機能作成、Breeze認証機能追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
 
 class PostController extends Controller
 {
@@ -18,4 +20,36 @@ class PostController extends Controller
         //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
     }
     
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+   public function edit(Post $post)
+   {
+       return view('posts.edit')->with(['post' => $post]);
+   }
+   
+   public function update(PostRequest $request, Post $post)
+   {
+       $input_post = $request['post'];
+       $post->fill($input_post)->save();
+
+       return redirect('/posts/' . $post->id);
+   }
+   
+   public function delete(Post $post)
+   {
+       $post->delete();
+       return redirect('/');
+   }
+   
+   public function create(Category $category)
+   {
+       return view('posts.create')->with(['categories' => $category->get()]);
+   }
+    
 }
+?>
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    
+    protected $fillable = [
+    'title',
+    'body',
+    'category_id'
+    ];
     
     public function getByLimit(int $limit_count = 10)
     {
@@ -15,9 +23,17 @@ class Post extends Model
         //updated_atで降順に並べたあと、limitで件数制限をかける
     }
     
-    public function getPaginateByLimit(int $limit_count = 10)
+    public function getPaginateByLimit(int $limit_count = 5)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+    
+    //「1対多」の関係なので単数系に
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    
+ 
 }
